c66f1e7c5c8ebae474539cf7c3b166b9241de56b
sanpele <cjaw426@gmail.com>
2021-09-04 13:31:05 -0500

    created new general index, while grouping content into years

---
+++ public_html/fall_2021/index.php
@@ -0,0 +1,5 @@
+<?php 
+
+	echo '<p> Hello there 2021, nice to see ya';
+
+?>
---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$header = fopen("header.html", "r") or die("unable to open file D;");
+
+echo fread($header, filesize("header.html"));
+
+//echo '<p>hello world, im gonna getcha</p>';
+
+$footer = fopen("footer.html", "r") or die("unable to open file you human");
+
+echo fread($footer, filesize("footer.html"));
+
+?>
