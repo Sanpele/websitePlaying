45d263c48a8c0361ead2e5891d5ece1cef3e1d9f
sanpele <cjaw426@gmail.com>
2022-02-16 11:11:36 -0600

    added bulletin date and changed tostring to support html rows

--- public_html/btr_covid_scrape/covidDataObj.php
+++ public_html/btr_covid_scrape/covidDataObj.php
@@ -3,25 +3,30 @@
 class CovidData {
 
     public $id; // Unique identifer each day so we don't have repeated values for date.
-    public $date; // String date. YYYY-MM-DD
+    public $bulletin_date; // String date. YYYY-MM-DD
+    public $scraped_date; // String date. YYYY-MM-DD
     public $prov_test_rate; // provincial test rate %
     public $wpg_test_rate; // winnipeg test rate %
     public $todays_cases; // number of new cases recorded today
 
-    public function __construct($id, $date, $prov, $wpg, $today) {
+    public function __construct($id, $bul_date, $t_date, $prov, $wpg, $today) {
         $this->id = $id;
-        $this->date = $date;
+        $this->bulletin_date = $bul_date;
+        $this->scraped_date = $t_date;
         $this->prov_test_rate = $prov;
         $this->wpg_test_rate = $wpg;
         $this->todays_cases = $today;
     }
-
+    
     public function __toString() {
-        $out = "id				: " . $this->id . "<br>" . 
-		"provincial rate	: " . $this->prov_test_rate . "<br>" . 
-		"wpg rate			: " . $this->wpg_test_rate . "<br>" . 
-		"todays cases		: " . $this->todays_cases . "<br>" .
-        "date           	: " . $this->date . "<br>";
+        $out = "<tr>" . 
+        "<td>" . $this->id . "</td>" . 
+        "<td>" . $this->bulletin_date . "</td>" . 
+        "<td>" . $this->scraped_date . "</td>" . 
+        "<td>" . $this->prov_test_rate . "</td>" . 
+        "<td>" . $this->wpg_test_rate . "</td>" . 
+        "<td>" . $this->todays_cases . "</td>" . 
+        "</tr>";    
         return $out;
     }
 }
