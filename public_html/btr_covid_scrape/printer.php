c741670c22ee5cbdfef901e557cd1b686dbce924
sanpele <cjaw426@gmail.com>
2022-03-29 15:16:11 -0500

    Updating format and beginning refactor of index

---
+++ public_html/btr_covid_scrape/printer.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+
+    To consolidate the echoing of things into one location.
+
+*/
+
+function printAllTable($DB) {
+
+}
+
+function printHeader() {
+
+}
+
+function printFooter() {
+
+}
+
+
+?>
